140a69c454e2c2cb1ebe8a269ddcc3b464fc81ab
Friedrich Pfitzmann <friedrich@pfiss.com>
2017-05-13 09:56:26 +0200

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#nvim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim
+ln -sf `pwd`/nvim/init.vim ~/.config/nvim/
+
+echo "# Now for some manual stuff, sorry!"
+echo "## nvim"
+echo "To install the nvim plugins, open up vim and type ':PlugInstall'\n"
