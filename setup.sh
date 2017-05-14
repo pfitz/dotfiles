e3aa6a4f94731cd4c7c8b9b2404f8b45cee8e8ab
pfitz <info@pfiss.com>
2017-05-14 16:21:55 +0200

    Auto start nvim install

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 mkdir -p ~/.config/nvim
 ln -sf `pwd`/nvim/init.vim ~/.config/nvim/
 
+nvim +PlugInstall
+
 echo "# Now for some manual stuff, sorry!"
 echo "## nvim"
 echo "To install the nvim plugins, open up vim and type ':PlugInstall'\n"
