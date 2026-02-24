cd0164a3fac6713d047e99ed61642042e39eb623
Friedrich Pfitzmann <friedrich.pfitzmann@teaminternet.com>
2026-02-24 16:37:15 +0100

    feat: migrate fish config from symlink to chezmoi-managed files
    Replace dot_config/symlink_fish (which created a symlink to ~/dotfiles/fish)
    with directly managed files under dot_config/fish/. secrets.fish is excluded.

---
+++ dot_config/fish/completions/shell-genie.fish
@@ -0,0 +1,1 @@
+complete --command shell-genie --no-files --arguments "(env _SHELL_GENIE_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) shell-genie)" --condition "env _SHELL_GENIE_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) shell-genie"
