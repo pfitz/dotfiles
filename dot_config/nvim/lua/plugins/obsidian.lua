d4fc47146cbb76111127c656a917370ee677ddf7
Friedrich Pfitzmann <friedrich.pfitzmann@teaminternet.com>
2026-02-24 16:28:09 +0100

    chore: reconcile already-managed files with live system

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -3,21 +3,22 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
-    -- Required.
+    -- Required
     "nvim-lua/plenary.nvim",
 
-    -- see below for full list of optional dependencies 👇
+    -- Recommended for completion
+    "hrsh7th/nvim-cmp",
+
+    -- Recommended for search/quick-switch
+    "nvim-telescope/telescope.nvim",
+
+    -- Recommended for syntax highlighting
+    "nvim-treesitter/nvim-treesitter",
   },
+
   opts = {
+    -- Workspace configuration
     workspaces = {
       {
         name = "personal",
@@ -25,6 +26,104 @@ return {
       },
     },
 
-    -- see below for full list of options 👇
+    -- Daily notes configuration
+    daily_notes = {
+      folder = "daily",
+      date_format = "%Y-%m-%d",
+      alias_format = "%B %-d, %Y",
+      default_tags = { "daily-notes" },
+      template = nil, -- Set to "daily.md" if you create a template
+    },
+
+    -- Templates configuration
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- Custom substitutions for dynamic content
+      substitutions = {
+        yesterday = function()
+          return os.date("%Y-%m-%d", os.time() - 86400)
+        end,
+        today = function()
+          return os.date("%Y-%m-%d")
+        end,
+        tomorrow = function()
+          return os.date("%Y-%m-%d", os.time() + 86400)
+        end,
+      },
+    },
+
+    -- Completion settings
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+
+    -- Picker configuration (uses Telescope)
+    picker = {
+      name = "telescope.nvim",
+      note_mappings = {
+        new = "<C-x>",
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        tag_note = "<C-x>",
+        insert_tag = "<C-l>",
+      },
+    },
+
+    -- Where to put new notes
+    new_notes_location = "current_dir",
+
+    -- Keybindings (optional, can also be set in your main keymap config)
+    mappings = {
+      -- Smart "gf" to follow links
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle checkboxes
+      ["<leader>oc"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart enter (follow link or open reference)
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+
+    -- Additional settings
+    ui = {
+      enable = true,  -- Enable UI enhancements
+      checkboxes = {
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+      },
+    },
+  },
+
+  -- Additional keybindings via keys (LazyVim-style)
+  keys = {
+    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note" },
+    { "<leader>oo", "<cmd>ObsidianOpen<cr>", desc = "Open in Obsidian app" },
+    { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show backlinks" },
+    { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Insert template" },
+    { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Open today's daily note" },
+    { "<leader>oy", "<cmd>ObsidianYesterday<cr>", desc = "Open yesterday's note" },
+    { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Search notes" },
+    { "<leader>oq", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick switch notes" },
+    { "<leader>ol", "<cmd>ObsidianLinks<cr>", desc = "Show note links" },
+    { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link" },
   },
 }
