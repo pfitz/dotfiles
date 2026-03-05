644feb0eb21d92b0f7ee8379d5ae41f352478729
Friedrich Pfitzmann <friedrich.pfitzmann@teaminternet.com>
2026-03-05 11:55:20 +0100

    vim-tmux-navigator: proper LazyVim integration
    - init: set no_mappings (define our own) and no_wrap (don't wrap edges)
    - keys: override LazyVim default C-h/j/k/l window navigation
    - add desc for which-key visibility

--- dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,15 +1,15 @@
 return {
   "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-  },
+  cmd = { "TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateUp", "TmuxNavigateRight" },
+  init = function()
+    vim.g.tmux_navigator_no_mappings = 1 -- we define our own in keys below
+    vim.g.tmux_navigator_no_wrap = 1 -- don't wrap around pane edges
+  end,
   keys = {
-    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+    -- Overrides LazyVim default <C-h/j/k/l> window navigation
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left (tmux-aware)" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down (tmux-aware)" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate up (tmux-aware)" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right (tmux-aware)" },
   },
 }
